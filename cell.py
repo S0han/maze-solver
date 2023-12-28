cfcf7c1d2c74f3ac2fdb20317a8b245f2ba7c19c
Kane Pereira <kane.pereira18@gmail.com>
2023-12-27 19:16:34 -0500

    finish draw_move() method on cell

--- cell.py
+++ cell.py
@@ -30,3 +30,44 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo = False):
+        if self._win is None:
+            return
+        fill_color = "red"
+        if undo:
+            fill_color = "black"
+        
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+
+        dest_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        dest_y_mid = (to_cell._y1 + to_cell._y2) / 2
+
+        # up
+        if self._y1 < to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(dest_x_mid, to_cell._y2), Point(dest_x_mid, dest_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # down
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(dest_x_mid, dest_y_mid), Point(dest_x_mid, to_cell._y))
+            self._win.draw_line(line, fill_color)
+
+        # left
+        elif self._x1 > to_cell._x1:
+            line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(dest_x_mid, dest_y_mid), Point(to_cell._x2, dest_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # right
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_cell._x1, dest_y_mid), Point(dest_x_mid, dest_y_mid))
+            self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from cell import Cell
 def main():
     win = Window(800, 600)
     
-    box = Cell(win)
-    box.has_bottom_wall = False
-    box.draw(50, 50, 100, 100)
+    a = Cell(win)
+    a.has_bottom_wall = False
+    a.draw(50, 50, 100, 100)
+
+    b = Cell(win)
+    b.has_left_wall = False
+    b.has_right_wall = False
+    b.draw(150, 150, 200, 200)
+
 
     win.wait_for_close()
 
